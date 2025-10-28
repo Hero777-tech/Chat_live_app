68c417428cb199d8beb264f14130e414ade7770d
Aditya Nath <pro.codenow@gmail.com>
2025-10-28 11:18:53 +0530

    first commit

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,10 @@
+// src/lib/axios.js
+import axios from "axios";
+
+// 💡 Change 1: Set the full HTTPS URL for production
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:3000/api" 
+    : "https://hero-chat-app-ien8.onrender.com/api", // 👈 CHANGED
+  withCredentials: true,
+});
